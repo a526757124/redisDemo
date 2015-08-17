f552869fb19031f8896794954eb8d6808ab4f15b
a526757124 <lijun526757124@163.com>
2015-08-17 17:38:36 +0800

    add adapter

---
+++ RedisSessionOwin/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedisSessionOwin
+{
+    class Startup
+    {
+        internal void Configuration(Microsoft.Owin.Builder.AppBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
